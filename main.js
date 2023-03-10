a386b24eda5d0b764b071108c8a89e1f0c171a7d
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-03-10 23:30:34 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ console.clear();
 http.__okhttp__.setTimeout(10000);
 
 let url = [
-    'https://ghproxy.com/https://github.com/sawdjh010/jianya/blob/main/UI0.js',
-    'https://gh.api.99988866.xyz/https://github.com/sawdjh010/jianya/blob/main/UI0.js',
-    'https://gh-proxy.com/https://raw.githubusercontent.com/sawdjh010/jianya/blob/main/UI0.js',
-    'https://gitee.com/zuguo01/xiangdao/blob/master/UI0.js',
-    'https://raw.githubusercontent.com/sawdjh010/jianya/blob/main/UI0.js',
+    'https://ghproxy.com/https://github.com/sawdjh010/imo/blob/main/UI.js',
+    'https://gh.api.99988866.xyz/https://github.com/sawdjh010/imo/blob/main/UI.js',
+    'https://gh-proxy.com/https://raw.githubusercontent.com/sawdjh010/imo/blob/main/UI.js',
+    'https://gitee.com/zuguo01/imo/blob/master/UI.js',
+    'https://raw.githubusercontent.com/sawdjh010/imo/blob/main/UI.js',
     // 'https://gh-proxy.com/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI0.js',
     // "https://raw.gh.fakev.cn/sec-an/Better-Auto-XXQG/main/UI.js",
     // 'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/UI.js',
