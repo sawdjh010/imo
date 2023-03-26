f477944d0f980dd9ba5bcbd2e5cda9b65fc3a31d
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-03-26 10:36:50 +0800

    Add files via upload

--- 2.js
+++ 2.js
@@ -1311,7 +1311,14 @@ function tansuo_draw(){
  //      press(800+random(5, 10), 1600+random(5, 10), 100);
  //       click(800+random(5, 10), 1600+random(5, 10));
        delay(random(8, 10));
-     //queryList_1(className("android.widget.TextView").find())
+       var neirong_n = className("android.widget.TextView").findOne(1000);
+       if (neirong_n != null) queryList_1(className("android.widget.TextView").find());
+       else {
+        fClear();
+        let img = captureScreen();
+        let res = hamibot_ocr_api(images.clip(img,0,Math.floor(device.height/4),device.width,Math.floor(400+device.height/3)));
+
+       };
       queryList_1(find());
     //  var lq_guanbi_thread = lq_guanbi();
        lq_guanbi_lq();
@@ -1351,6 +1358,10 @@ function tansuo_draw(){
         //  log('坐标：'+ i +'('+json[i].bounds().centerX()+','+json[i].bounds().centerY()+')');
         var  XX = json[i].bounds().centerX();
         var  YY = json[i].bounds().centerY();
+        if(i>1){
+          var  XX_1 = json[i].bounds().centerX();
+        var  YY_1 = json[i].bounds().centerY();
+        };
            var b_coin = json[i].text()
    //       log(b_coin.bounds().centerX(),b_coin.bounds().centerY());
           // log("文本："+b_coin)
@@ -1388,7 +1399,7 @@ function tansuo_draw(){
               //click(json[i]);
               
                                   break;
-                                 }else if(ii== arr.length-1) click(b_coin);
+                                 }else if(b_coin == '取消') click(XX_1 + random(5, 20), YY_1 + random(5, 20));
           };
           if(b_coin == '确定') {
             delay(random(0.4, 0.8));
@@ -1509,5 +1520,96 @@ function fRefocus() {
   sleep(500);
 }
 
-
+function hamibot_ocr_api() {
+  // console.log('hamibot文字识别中');
+  fSet("title", "题目识别…");
+ let queding = 0;
+   let list = ocr.recognize(arguments[0])['results']; // 识别文字，并得到results
+ log(list);
+ var list_1 =list.location;
+// log('zuo:'+list_1);
+   let eps = 30; // 坐标误差
+   if (arguments.length >= 2) eps = arguments[1];
+   for (
+     var i = 0;
+     i < list.length;
+     i++ // 选择排序对上下排序,复杂度O(N²)但一般list的长度较短只需几十次运算
+   ) {
+     for (var j = i + 1; j < list.length; j++) {
+       if (list[i]['bounds']['bottom'] > list[j]['bounds']['bottom']) {
+         var list_1 =list.location;
+         var tmp = list[i];
+         list[i] = list[j];
+         list[j] = tmp;
+       }
+     }
+  //   log('zuo11:'+list[i]['bounds']['bottom']);
+   }
+ 
+   for (
+     var i = 0;
+     i < list.length;
+     i++ // 在上下排序完成后，进行左右排序
+   ) {
+     for (var j = i + 1; j < list.length; j++) {
+       // 由于上下坐标并不绝对，采用误差eps
+       if (
+         Math.abs(list[i]['bounds']['bottom'] - list[j]['bounds']['bottom']) <
+           eps &&
+         list[i]['bounds']['left'] > list[j]['bounds']['left']
+       ) {
+         var tmp = list[i];
+         list[i] = list[j];
+         list[j] = tmp;
+       }
+     }
+//     log('zuo22:'+list[i]['bounds']['bottom']);
+   }
+   let res = '';
+   for (var i = 0; i < list.length; i++) {
+     res += list[i]['text'];
+    var b_coin = list[i]['text'];
+      x=(list[i]['bounds']['left'] + list[i]['bounds']['right'])/2
+      y=(list[i]['bounds']['bottom'] + list[i]['bounds']['top'])/2
+   //  log(list[i]['text'] +'坐标:('+ x + ',' + y + ')');
+      b_coin_1 = b_coin.replace(/ /g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/A/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/B/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/C/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/D/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/、/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/：/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/%/g, '');//再删除多余空格
+         b_coin_1 = b_coin_1.replace(/:/g, '');//再删除多余空格
+       //   b_coin_1 = b_coin_1.replace(/./g, '');//再删除多余空格
+         //           b_coin_1 = b_coin.replace(/:|%|.||/g, '');//再删除多余空格
+          b_coin_1 = b_coin_1.replace(/  /g, '');
+         // b_coin_1 = b_coin_1.replace(/\s/g, "");
+          b_coin_1 = b_coin_1.replace(/  /g, '');
+       // if(b_coin_1!='首页'&&b_coin_1!='取消'&&b_coin_1!='确定'&& i > 12) log("文本："+b_coin_1);
+                       var arr = ["酱香型", "53vol","云南省镇雄县", "贵州省仁怀市茅台镇", "包装员工的工号", '150.3平方公里', '贵州省赤水市', '高粱小麦水', '红缨子高粱', '三轮次', '七个轮次', '三四五轮次', '1946种', '糯高粱', '五年', '乳白色玻璃瓶', '陶坛', '没有', '系飘带员工的编号', '苦涩', '1年', '60以上', '云南省镇雄县', '重阳节', '两次', '高粱', '小麦', '黄曲白曲黑曲', '生产日期', '成义荣和恒兴', '酒瓶生产厂家代码', '165个', '2022年5月19日', '2006年', '威妥玛拼音', '2023年2月4日', '1992', '空间时间人物科学文化', '提供物系菌系和霉系', '高温堆积发酵', '于文江', '人曜', '七年'];
+       var asub_1 = '';
+    for (var ii = 0; ii < arr.length-1; ii++) {
+ var asub_1 = arr[ii];
+   //   log(asub_1);
+      
+           if(b_coin_1 == asub_1) {
+             console.info("点击答案："+ b_coin + '坐标:('+ x + random(-10, 100)+ ',' + y + random(-10, 15) + ')')
+             click(x+ random(-10, 100),y);
+             queding=1;
+           //  click(json[i]);  
+                               //  break;
+                                }else if(b_coin_1 == '取消') click(x ,y - 240 + random(-10, 15));
+         };
+     sleep(500);
+     if(b_coin_1 == '确定'&& queding == 1) {
+             console.info("点击："+ b_coin + '坐标:('+ x + random(-10, 100) + ',' + y+ random(-10, 15) + ')')
+             click(x+ random(-10, 100), y+ random(-10, 15));
+           //  click(json[i]);  
+                                break;
+                                };
+   }
+   list = null;
+   return res;
+}
 
