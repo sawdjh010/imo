2b3e3040c4fa17397ad3854861db2d29fa16702b
sawdjh010 <124690491+sawdjh010@users.noreply.github.com>
2023-05-19 00:05:56 +0800

    Add files via upload

--- 2.js
+++ 2.js
@@ -1381,8 +1381,16 @@ function tansuo_draw(){
      var path_jp=0;
       while (true && path_jp<7){
  // try {
-       let img_small_kaishi = images.read(path_jpg_2);
-     let img_big_kaishi = captureScreen();
+  try {
+    var img_small_kaishi = images.read(path_jpg_2);
+  } catch (e) {
+    var img_small_kaishi = images.read(path_jpg_2);
+  }
+  try {
+    var img_big_kaishi = captureScreen();
+  } catch (error) {
+    var img_big_kaishi = captureScreen();
+  }     
      sleep(700);
     //  let result_0 = images.matchTemplate(img_big_kaishi, img_small_kaishi, {
     //    max: 1
